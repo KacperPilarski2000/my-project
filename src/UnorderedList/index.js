2a60648644f12c4adcf71c4f2b517d4668cd35b2
kacper <xxxxbaobabxxxx@gmail.com>
2024-02-05 18:57:20 +0100

    first commit

---
+++ src/UnorderedList/index.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+const UnorderedList = (props) => (
+  <ul className="ListItem__division">
+    {props.tasks.map(task => (
+      <li
+        className={`listItem ${
+          task.done && props.hideDoneTasks ? "listItem--hidden" : ""
+        }`}
+      >
+        <button className="listItem__checkButton">
+          {task.done ? "✔" : ""}
+        </button>
+        <span
+          className={`listItem__paragraph ${
+            task.done ? "listItem__task--done" : ""
+          }`}
+        >
+          {task.content}
+        </span>
+        <button className="listItem__button">🗑️</button>
+      </li>
+    ))}
+  </ul>
+);
+
+export default UnorderedList;
