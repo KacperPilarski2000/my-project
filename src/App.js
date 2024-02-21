d0ba6ca4533f9e0fca7e95dfd919d0007a399ea6
Kacper Pilarski <xxxxbaobabxxxx@gmail.com>
2024-02-21 19:22:11 +0100

    add intaraction to check, delete, and hide button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./Form";
 import UnorderedList from "./UnorderedList";
 import Buttons from "./Buttons";
-import {Division} from "./Division";
+import { Division } from "./Division";
 import Header from "./Header";
 import Main from "./Main";
-import React, { useState } from "react";
-
-const tasks = [
-  { id: 1, content: "Umyć okna", done: false },
-  { id: 2, content: "Zrobić sernik", done: true },
-];
-
-
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "Umyć okna", done: false },
+    { id: 2, content: "Zrobić sernik", done: true },
+  ]);
 
   const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
+    setHideDone((hideDone) => !hideDone);
+  };
+
+  const removeTasks = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleDone = (id) => {
+    setTasks((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        }
+        return task;
+      })
+    );
   };
 
   return (
@@ -28,11 +40,21 @@ function App() {
         <Division title="Dodaj nowe zadanie" content={<Form />} />
         <Division
           title="Lista zadań"
-          extraContent={<Buttons tasks={tasks}
-          hideDone={hideDone}
-          toggleHideDone={toggleHideDone}
-          />}
-          content={<UnorderedList tasks={tasks} hideDone={hideDone} />}
+          content={
+            <UnorderedList
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleDone={toggleDone}
+              removeTasks={removeTasks}
+            />
+          }
+          extraContent={
+            <Buttons
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleHideDone={toggleHideDone}
+            />
+          }
         />
       </section>
     </Main>
